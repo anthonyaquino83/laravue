58c0a88aba4d1125166fec82cf5222e2121b2f52
anthonyaquino <anthonyjefferson@gmail.com>
2024-06-25 19:35:03 -0300

    initial

---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import IncrementCounter from "./components/IncrementCounter.vue";
+</script>
+
+<template>
+    <h1>How To Install Vue 3 in Laravel 10 : ZWebCourses :)</h1>
+    <IncrementCounter></IncrementCounter>
+</template>
